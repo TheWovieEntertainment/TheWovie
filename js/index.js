ff28f393dcc31fea235a6bf6e72e5c31aec5f3d5
Innovation <lt1550919167@163.com>
2017-06-21 13:30:44 +0800

    注册登录界面基本完成;增加广告栏随机背景

---
+++ js/index.js
@@ -0,0 +1,29 @@
+//   广告栏上滑消失
+  $(document).ready(function(){
+    setTimeout(function(){$("#ad").slideUp(600);
+    },1200);
+    
+});
+
+// 广告栏随机背景颜色
+function bg(){
+    var r=Math.floor(Math.random()*150+50);
+    var g=Math.floor(Math.random()*150+50);
+    var b=Math.floor(Math.random()*150+50);
+    return  "rgb("+r+','+g+','+b+")";
+}
+function change(){
+    // var bgcolor = Array("black","aqua","cadetblue","indianred","lightblue","#560764","#1D3E53","#B1D056","#1C1124","#EF9037");
+    // var rand = parseInt(Math.random() * 10);
+    // document.getElementById("ad").style.backgroundColor = bgcolor[rand];
+    document.getElementById("ad").style.backgroundColor = bg();
+}
+
+//点击 加载更多
+$(document).ready(function(){
+    $(".loadMore").click(function(){
+        $(".wrapper").css("height","2000px");
+        $(".cover").css("display","block");
+        $(".loadMore").css("display","none");
+    });
+});
