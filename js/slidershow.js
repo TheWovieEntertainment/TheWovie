209eb909fb0efd7ed00ba97014160e5218ba3ae7
Innovation <lt1550919167@163.com>
2017-06-21 21:32:48 +0800

    index导航栏强行增加二级菜单;增加用户注册界面用户名正则表达式

---
+++ js/slidershow.js
@@ -0,0 +1,91 @@
+$(function () {
+            var container = $('#container');
+            var list = $('#list');
+            var buttons = $('#buttons span');
+            var prev = $('#prev');
+            var next = $('#next');
+            var index = 1;
+            var len = 5;
+            var interval = 3000;
+            var timer;
+
+
+            function animate (offset) {
+                var left = parseInt(list.css('left')) + offset;
+                if (offset>0) {
+                    offset = '+=' + offset;
+                }
+                else {
+                    offset = '-=' + Math.abs(offset);
+                }
+                list.animate({'left': offset}, 300, function () {
+                    if(left > -200){
+                        list.css('left', -942 * len);
+                    }
+                    if(left < (-942 * len)) {
+                        list.css('left', -942);
+                    }
+                });
+            }
+
+            function showButton() {
+                buttons.eq(index-1).addClass('on').siblings().removeClass('on');
+            }
+
+            function play() {
+                timer = setTimeout(function () {
+                    next.trigger('click');
+                    play();
+                }, interval);
+            }
+            function stop() {
+                clearTimeout(timer);
+            }
+
+            next.bind('click', function () {
+                if (list.is(':animated')) {
+                    return;
+                }
+                if (index == 5) {
+                    index = 1;
+                }
+                else {
+                    index += 1;
+                }
+                animate(-942);
+                showButton();
+            });
+
+            prev.bind('click', function () {
+                if (list.is(':animated')) {
+                    return;
+                }
+                if (index == 1) {
+                    index = 5;
+                }
+                else {
+                    index -= 1;
+                }
+                animate(942);
+                showButton();
+            });
+
+            buttons.each(function () {
+                 $(this).bind('click', function () {
+                     if (list.is(':animated') || $(this).attr('class')=='on') {
+                         return;
+                     }
+                     var myIndex = parseInt($(this).attr('index'));
+                     var offset = -942 * (myIndex - index);
+
+                     animate(offset);
+                     index = myIndex;
+                     showButton();
+                 })
+            });
+
+            container.hover(stop, play);
+
+            play();
+
+        });
