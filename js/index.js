0b7f3ab95f14a0425ee0ca2ddfb0e4051ecb1642
DESKTOP-Q4IJE0N\Innovation <lt1550919167@163.com>
2017-07-03 16:27:43 +0800

    修改顶部广告位，页面刷新后广告栏下滑显示

--- js/index.js
+++ js/index.js
@@ -1,9 +1,3 @@
-//   广告栏上滑消失
-  $(document).ready(function(){
-    setTimeout(function(){$("#ad").slideUp(600);
-    },1200);
-    
-});
 
 // 广告栏随机背景颜色
 function bg(){
@@ -12,11 +6,14 @@ function bg(){
     var b=Math.floor(Math.random()*150+50);
     return  "rgb("+r+','+g+','+b+")";
 }
+// 广告栏滑动出现消失
 function change(){
     // var bgcolor = Array("black","aqua","cadetblue","indianred","lightblue","#560764","#1D3E53","#B1D056","#1C1124","#EF9037");
     // var rand = parseInt(Math.random() * 10);
     // document.getElementById("ad").style.backgroundColor = bgcolor[rand];
     document.getElementById("ad").style.backgroundColor = bg();
+    setTimeout(function(){$("#ad").slideDown(680);},0);
+    setTimeout(function(){$("#ad").slideUp(680);},1600);
 }
 
 
@@ -33,6 +30,7 @@ $(document).ready(function(){
             
     });
 });
+
 
 //点击 加载更多
 $(document).ready(function(){
